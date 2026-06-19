fed7d013e688a56a0c509ea21203ba134fa6f972
PIYUSH PANDEY <pandeypiyush9372@gmail.com>
2026-06-19 20:09:36 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,14 @@
+function appendValue(value){
+    document.getElementById("display").value+=value;
+}
+function calculate(){
+    let result = eval(document.getElementById("display").value);
+    document.getElementById("display").value = result;
+}
+function clearDisplay(){
+    document.getElementById("display").value="";
+}
+function backspace(){
+    let display = document.getElementById("display");
+    display.value=display.value.slice(0,-1);
+}
